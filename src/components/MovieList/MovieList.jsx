2b97fc702b20d6ca036debccf942eee23597214c
Vlad-Andrei Mocan <Vlad.Mocan@cognizant.com>
2026-02-13 17:44:07 +0200

    feat:search functionality implemented

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,11 +1,18 @@
+import { useOutletContext } from "react-router-dom";
 import MovieCard from "../MovieCard/MovieCard";
 import styles from "./MovieList.module.css";
 
 export default function MovieList({ moviesData, setSelectedMovie }) {
+  const { searchQuery } = useOutletContext();
+
+  const filteredMovies = moviesData.filter((movie) =>
+    movie.title.toLowerCase().includes(searchQuery.toLowerCase()),
+  );
+
   return (
     <ul className={styles.moviesContainer}>
-      {moviesData.map((movie) => (
-        <li key={movie.id}>
+      {filteredMovies.map((movie) => (
+        <li key={movie.id} className={styles.listItem}>
           <MovieCard movieData={movie} setSelectedMovie={setSelectedMovie} />
         </li>
       ))}
